bdc9814ba1177f5352fced70ff27dcde0b990c27
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2024-03-13 15:48:17 -0300

    add q3

---
+++ EET50/lab1_q3.m
@@ -0,0 +1,29 @@
+close all
+arquivo_audio = 'audio.wav';
+[x, fs] = audioread(arquivo_audio);
+
+%sound(x, fs); %clear sound
+
+% o sinal é estereo, portanto temos duas colunas. Selecionando apenas uma
+% delas para trabalhar
+
+x = x(:, 1);
+
+N = length(x); 
+X = fft(x); 
+X_mag = abs(fftshift(X))/N; 
+f = (-N/2:N/2-1)*(fs/N);
+
+t = (-N/2:N/2-1)*(1/fs);
+
+SIR = 0.0001;
+sinc_t = (1000 * sinc(t*1000).*cos(2*pi*1000*t))';
+x_com_interferencia = x + 1/SIR*sinc_t;
+
+%sound(x_com_interferencia, fs);
+
+Y=fft(x_com_interferencia);
+Y_mag = abs(fftshift(Y))/N; 
+
+figure;
+plot(f, Y_mag);

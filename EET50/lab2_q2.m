faf61ea51b6976d9c719feb154b7e352bf55cd17
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2024-04-16 16:08:25 -0300

    feat: corrigido lab2 q2

--- EET50/lab2_q2.m
+++ EET50/lab2_q2.m
@@ -1,43 +1,35 @@
-%% Gerar o Seno
-clear all
+%% Leitura do arquivo de áudio
+%clear all
 arquivo_audio = 'audio.wav';
 [signal, f_s] = audioread(arquivo_audio);
-sinal_seno = transpose(signal(:,1));
+sinal_audio = transpose(signal(:,1));
 
 frequencia_seno = 10*1000; %freq de corte
-t = [0:1/f_s:(length(sinal_seno)-1)/f_s];
+t = [0 : 1/f_s : (length(sinal_audio)-1)/f_s];
 
 subplot(3,1,1);
-plot(t,sinal_seno,'black');
-title('Message Signal');
-xlabel('Time(s) ---->')
-ylabel('Amplitude(V) ---->')
-legend('Message Signal ---->');
+plot(t,sinal_audio,'black');
+title('Sinal Mensagem');
+xlabel('Tempo (s)')
+ylabel('Amplitude (V)')
+legend('Sinal Mensagem');
 grid on
 
-%% Amostrando o seno
-T_s = 1/f_s;
-%N = length(t);
-
-%t_amostra = [0 : T_s : 10/frequencia_seno];
-%f_digital = 2*pi*frequencia_seno/f_s;
-
-%sinal_amostrado = sin(f_digital .* n);
 %% Quantizando o sinal
 num_bits = 8;
 L = 2^num_bits;
-max_sen = max(sinal_seno);
-min_sen = min(sinal_seno);
+max_sinal = max(sinal_audio);
+min_sinal = min(sinal_audio);
 
-delta = (max_sen - min_sen) / L;
-mapa_q = [min_sen:delta:max_sen];
-cod_q = [min_sen-delta/2:delta:max_sen+delta/2];
+delta = (max_sinal - min_sinal) / L;
+mapa_q = [min_sinal : delta : max_sinal];
+cod_q = [min_sinal-delta/2 : delta : max_sinal+delta/2];
 
-[index, quants] = quantiz(sinal_seno,mapa_q,cod_q);
+[index, quants] = quantiz(sinal_audio,mapa_q,cod_q);
 
 
 for i = 1:length(quants)
-    if quants(i) < min_sen
+    if quants(i) < min_sinal
        quants(i) = quants(i) + delta;
        index(i) = index(i) + 1;
     end
@@ -47,21 +39,20 @@ end
 
 subplot(3,1,2);
 plot(t,quants);
-title('Quantized Signal');
-xlabel('Samples ---->')
-ylabel('Amplitude(V) ---->')
-legend('Quantized Signal ---->');
+title('Sinal Quantizado');
+xlabel('Amostras')
+ylabel('Amplitude (V)')
+legend('Sinal Quantizado');
 grid on
 
-%% Encode
-Rb = frequencia_seno * num_bits;
+%% Codificação
+Rb = f_s * num_bits;
 T_enc = 1/Rb;
-T_por_bit = T_enc/num_bits;
 
 y = de2bi(index);
 
 y = reshape(y',1,[]);
-t_enc = [0:1/(f_s)/num_bits:(length(sinal_seno)-1)/f_s+1/(f_s)/num_bits];
+t_enc = [0 : T_enc : length(sinal_audio)*T_enc*num_bits - T_enc];
 
 subplot(3,1,3);
 plot(t_enc,y,'red');
@@ -77,8 +68,15 @@ text (0.5, 1,'\bf Pulse Code Modulation ','HorizontalAlignment','center','Vertic
 
 %% Demodulação
 % parte do y para mq
-mq = quants;
-% lowpass
+
+mq = zeros(length(y)/num_bits,1)';
+volta = bit2int(y',num_bits,false)'; % false porque o bit mais significante é o ultimo
+
+for i = 1:length(volta)
+    mq(i) = (min_sinal + delta/2) + volta(i)*delta;
+end
+
+% Passa baixas
 filtrado = lowpass(mq,frequencia_seno,f_s,ImpulseResponse="iir",Steepness=0.7);
 figure
 plot(t, filtrado)
